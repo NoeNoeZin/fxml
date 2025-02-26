6261c5084ddb764b1a269a479043431ae46e2e86
NoeNoeZin <nonozin76842@gmail.com>
2025-02-26 10:28:05 +0630

    added files

---
+++ src/application/Main.java
@@ -0,0 +1,32 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent; 
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class Main extends Application {
+  @Override
+  public void start(Stage stage) throws IOException {
+    Parent root = FXMLLoader.load(getClass().getResource("crypto.fxml"));
+    
+    Scene scene = new Scene(root);
+    stage.initStyle(StageStyle.TRANSPARENT);
+    
+//    String css = this.getClass().getResource("application.css").toExternalForm();
+//    scene.getStylesheets().add(css);
+    
+    stage.setScene(scene);
+    stage.show();
+  }
+  
+  public static void main(String[] args) {
+    launch(args);
+  }
+  
+
+}
